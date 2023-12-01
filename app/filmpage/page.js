84d86c3545eb2384f52a7722aa7e3936e881786f
Oitlovich <Oitlovich@yandex.ru>
2023-12-01 11:14:59 +0400

    commit

--- app/filmpage/page.js
+++ app/filmpage/page.js
@@ -5,15 +5,18 @@ import Footer from '../components/footer'
 export default function page() {
   return (
     <section className='filmpage'>
-        <div className='film_header'></div>
+      <div className='film_black_header'>
+        <div className='film_header'>
+          <Image className='film_header_image' src='/images/image 1 (1).png' alt='' width={1900} height={730}/>
+        </div>
         <div className='film_header_black'></div>
+      </div>
         <div className='film_nameblock'>
           <div className='film_name'>
             <h1>
               Локи
             </h1>
           </div>
-        <div className='red_string'></div>
         </div>
         <div className='film_desc'>
           <div className='film_left_side'>
@@ -25,7 +28,6 @@ export default function page() {
               <div>
                 <h2>Локи</h2>
                 <p>Американский сериал, созданный Майклом Уолдроном для стримингового сервиса Disney+ и основанный на одноимённом персонаже из Marvel Comics</p>
-                <div className='red_string'></div>
               </div>
             </div>
             <div className='rs_rating'>
@@ -41,9 +43,38 @@ export default function page() {
                 <div><p>PG-13</p></div>
                 <div><p>8 сезонов</p></div>
                 <div><p>2021</p></div>
+                <p>Галерея</p>
               </div>
             </div>
-            
+            <div className='rs_details'>
+              <h1>Детали</h1>
+              <p><b>Жанр:</b>Экшн, Фентези, Драма</p>
+              <p><b>Страна:</b>США</p>
+              <p><b>Режиссёр:</b>Алан Тейлор</p>
+              <p><b>Сценарист:</b>Джордж Мартин</p>
+            </div>
+            <div className='rs_acters'>
+              <h1>Актёры</h1>
+              <p>Том Хиддлстон</p>
+              <p>Оуэн Уилсон</p>
+              <p>София ди Мартино</p>
+            </div>
+            <div>
+            <video></video>
+            <Image></Image><Image></Image>
+            </div>
+          </div>
+          <div>
+          <div>
+            <h1>
+              Описание
+            </h1>
+          </div>
+          <div>
+            <p>
+              Бог коварства попадает в УВА и находит там новых друзей, а что будет дальше мы прямо сейчас м узнаем.
+            </p>
+          </div>
           </div>
         </div>
         <div className=''></div>
