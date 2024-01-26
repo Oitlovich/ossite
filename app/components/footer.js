08161fd554cc703b65c0be2103de8fa6b8083e40
Oitlovich <Oitlovich@yandex.ru>
2024-01-26 16:16:17 +0400

    commit

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,22 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <footer>
-      
+      <div className='container'>
+        <div className='footer_column'>
+          <a href='#'><Image></Image></a>
+          <a href='#'>Главная</a>
+          <a href='#'>Каталог</a>
+        </div>
+        <div className='footer_column'>
+          <a href='#'>Регистрация</a>
+          <a href='#'>Авторизация</a>
+        </div>
+        <div className='footer_column'>
+          <a href=''>Политика конфиденциальности</a>
+          <a href=''>Обратная связь</a>
+          <a>© 2024 Все права защищены</a>
+        </div>
+      </div>
     </footer>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,22 +3,97 @@ import Header from './components/header'
 import Footer from './components/footer'
 export default function page() {
   return (
-    <section className='main_page'>
-      <div className='left_part'>
-        <div className='Logo_text'>
-          <p><span>O</span>itlovich</p>
-          <div></div>
-          <p><span>Studio</span></p>
+    <main>
+      <section className='main_page'>
+        <div className='left_part container'>
+          <div className='Logo_text'>
+            <p><span>O</span>itlovich</p>
+            <div></div>
+            <p><span>Studio</span></p>
+          </div>
+          <div><p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века</p></div>
+          <div className='left_part_buttons'>
+            <a href=''>Регистрация</a>
+            <a href=''>Каталог</a>
+          </div>
         </div>
-        <div>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века</div>
-        <div className='left_part_buttons'>
-          <a href=''>Регистрация</a>
-          <a href=''>Каталог</a>
+        <div className='right_part container'>
+          <Image src="/images/micromainpage.png"></Image>
         </div>
-      </div>
-      <div className='right_part'>
-        <Image src="/images/micromainpage.png"></Image>
-      </div>
-    </section>
+      </section>
+      <section className='command'>
+        <div className='section_header container'>
+          <h1><span>К</span>оманда озвучки</h1>   
+        </div>
+        <div className='command_slider container'>
+          <div className='command_element'>
+            <Image></Image>
+            <p>Александр Копаев</p>
+          </div>
+          <div className='command_element'>
+            <Image></Image>
+            <p>Никита Мальцев</p>
+          </div>
+          <div className='command_element'>
+            <Image></Image>
+            <p>Сергей Малеев</p>
+          </div>
+        </div>
+      </section>
+      <section className='popular'>
+        <div className='popular_header container'>
+          <h1><span>П</span>опулярное</h1>
+          <a href='#'>Показать больше</a>   
+        </div>
+        <div className='popular_main container'>
+          <a href='#'>
+            <div className='popular_element container'>
+              <Image src="" width={200} height={342}></Image>
+              <p></p>
+              <p></p>
+            </div>
+          </a>
+          <a href='#'>
+            <div className='popular_element container'>
+              <Image src="" width={200} height={342}></Image>
+              <p></p>
+              <p></p>
+            </div>
+          </a>
+          <a href='#'>
+            <div className='popular_element container'>
+              <Image src="" width={200} height={342}></Image>
+              <p></p>
+              <p></p>
+            </div>
+          </a>
+          <a href='#'>
+            <div className='popular_element container'>
+              <Image src="" width={200} height={342}></Image>
+              <p></p>
+              <p></p>
+            </div>
+          </a>
+          <a href='#'>
+            <div className='popular_element container'>
+              <Image src="" width={200} height={342}></Image>
+              <p></p>
+              <p></p>
+            </div>
+          </a>
+        </div>
+      </section>
+      <section className='beginnings'>
+        <div className='left_part_beginnings container'>
+          <div className='section_header container'>
+            <h1><span>С</span>чего всё начиналось</h1>   
+          </div>
+          <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века</p>
+        </div>
+        <div className='right_part_beginnings container'>
+          <Image src="" width={333} height={436}></Image>
+        </div>
+      </section>
+    </main>
   )
 }
